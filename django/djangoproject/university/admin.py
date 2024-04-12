85f7d16134dfcc8b0eb993a6942b0118d0b2345b
JSVazz <144262237+JSVazz@users.noreply.github.com>
2024-04-11 20:55:58 -0800

    lab12, added models and admin to university

--- django/djangoproject/university/admin.py
+++ django/djangoproject/university/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Student, Course, Room, Enrollment
 
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(Room)
+admin.site.register(Enrollment)
--- django/djangoproject/university/models.py
+++ django/djangoproject/university/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Course(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.name} ({self.code})"
+
+
+class Room(models.Model):
+    id = models.AutoField(primary_key=True)
+    number = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"Room {self.number}"
+
+
+class Enrollment(models.Model):
+    id = models.AutoField(primary_key=True)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.student} enrolled in {self.course} in room {self.room}"
